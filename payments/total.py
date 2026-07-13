d6e6e836b49ad3ebb42ec479dae15048b53181ef
Amna <mohammadamna54@gmail.com>
2026-07-14 03:15:54 +0500

    test pipeline trigger Tue Jul 14 03:15:54 AM PKT 2026

--- payments/total.py
+++ payments/total.py
@@ -1,5 +1,6 @@
 def calculate_total(cart_items, discount=0):
     total = 0
     for item in cart_items:
-        total = item["price"] * item["quantity"]  # BUG: should be +=
+        total = item["price"] * item["quantity"]  # BUG: should be += not =
+        # TODO: fix this bug
     return total * (1 - discount)
