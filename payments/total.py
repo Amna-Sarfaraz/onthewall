cfadfb44f3624dea5b2933e4d273007dd6f34f9d
Amna <mohammadamna54@gmail.com>
2026-07-14 17:12:02 +0500

    add buggy calculator Tue Jul 14 05:12:02 PM PKT 2026

--- payments/total.py
+++ payments/total.py
@@ -1,6 +1,5 @@
 def calculate_total(cart_items, discount=0):
     total = 0
     for item in cart_items:
-        total = item["price"] * item["quantity"]  # BUG: should be += not =
-        # TODO: fix this bug
+        total = item["price"] * item["quantity"]  # BUG: should be +=
     return total * (1 - discount)
